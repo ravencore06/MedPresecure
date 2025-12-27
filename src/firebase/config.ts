893378dc466bb4e8e730b38a355d24d8b5a01257
Srinidhi Sadhanala <srinidhisadhanala@gmail.com>
2025-12-27 18:39:50 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3189639860-4ac7d",
+  "appId": "1:424813661602:web:a4a54acb72f6dd68ab76fb",
+  "apiKey": "AIzaSyDRZjPJBiQTdmTs4UT8YXlEhJL3JbPNff8",
+  "authDomain": "studio-3189639860-4ac7d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "424813661602"
+};
