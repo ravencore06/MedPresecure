7750f8df426d52e54d1e23aa5a480d89697bf30c
Srinidhi Sadhanala <srinidhisadhanala@gmail.com>
2026-01-03 09:49:13 +0000

    fix this

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-3189639860-4ac7d",
   "appId": "1:424813661602:web:a4a54acb72f6dd68ab76fb",
+  "storageBucket": "studio-3189639860-4ac7d.appspot.com",
   "apiKey": "AIzaSyDRZjPJBiQTdmTs4UT8YXlEhJL3JbPNff8",
   "authDomain": "studio-3189639860-4ac7d.firebaseapp.com",
   "measurementId": "",
